f6c08f243569fe1d548f0044ad51a12f85efe0cc
Chris Xiong <chrisxiongyang@yahoo.com>
2017-12-19 14:56:14 -0800

    Added some position check functions to ball and dot

--- app/src/main/java/com/chrisx/ricochet/Ball.java
+++ app/src/main/java/com/chrisx/ricochet/Ball.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 
 public class Ball {
     public double x, y, size, angle, speed;
+    public double w, h; //canvas width/height
     public boolean moving, bounced;
     private Paint p;
 
@@ -16,15 +17,38 @@ public class Ball {
         angle = 0;
         speed = w*6/400;
 
+        this.w = w;
+        this.h = h;
+
         moving = bounced = false;
 
         p = new Paint(Paint.ANTI_ALIAS_FLAG);
         p.setColor(Color.WHITE);
     }
 
+    public void reset() {
+        x = w/2;
+        y = h/2;
+
+        moving = bounced = false;
+    }
+
     public void move() {
         x += Math.cos(angle) * speed;
         y += Math.sin(angle) * speed;
+    }
+
+    //check if ball is out of bounds
+    public boolean offScreen() {
+        return x < -size || x > w+size || y < h/2-w/2-size || y > h/2+w/2+size;
+    }
+    //check if ball is touching left or right walls
+    public boolean touchingLRWalls() {
+        return x < size/2 || x > w-size/2;
+    }
+    //check if ball is touching top or bottom walls
+    public boolean touchingUDWalls() {
+        return y < h/2-w/2+size/2 || y > h/2+w/2-size/2;
     }
 
     public void draw(Canvas c) {
--- app/src/main/java/com/chrisx/ricochet/Dot.java
+++ app/src/main/java/com/chrisx/ricochet/Dot.java
@@ -10,8 +10,7 @@ public class Dot {
     private Paint p;
 
     public Dot(double w, double h) {
-        size = w/10;
-        x = y = 0;
+        reset();
 
         this.w = w;
         this.h = h;
@@ -20,11 +19,16 @@ public class Dot {
         p.setColor(Color.rgb(25,149,173));
     }
 
+    public void reset() {
+        size = w*3/20;
+        x = y = 0;
+    }
+
     public void changeLocation(Ball b) {
         double dist;
         do {
             x = Math.random()*(w - size) + size/2;
-            y = Math.random()*(h - size) + size/2;
+            y = h/2 - w/2 + Math.random()*(w - size) + size/2;
             dist = Math.sqrt((b.x-x)*(b.x-x) + (b.y-y)*(b.y-y));
         } while (dist <= b.size + size);
     }
